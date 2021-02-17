76eca24f3a833f24b1c2f876adfd981d8edfce5d
Zriox <37074290+zriox@users.noreply.github.com>
2021-02-17 10:51:35 +0100

    New emojis

--- commands/clean.js
+++ commands/clean.js
@@ -12,10 +12,10 @@ async function run (message, args) {
     if (!message.member.roles.cache.some(r => r.id === '802951934406557738')) return
 
     let nbr = args.join("")
-    if (nbr === "" || nbr === undefined || isNaN(nbr)) return message.channel.send("<:ui_5:483262304868106251> Il faut indiquer le nombre de message à suprimer et écrire la commande ainsi `!clean x` (x étant le nombre de message à suprimer).")
-    if (nbr < 1) return message.channel.send("<:ui_5:483262304868106251> Le nombre de message à supprimer doit être supérieur à 0.")
+    if (nbr === "" || nbr === undefined || isNaN(nbr)) return message.channel.send("❌ Il faut indiquer le nombre de message à suprimer et écrire la commande ainsi `!clean x` (x étant le nombre de message à suprimer).")
+    if (nbr < 1) return message.channel.send("❌ Le nombre de message à supprimer doit être supérieur à 0.")
 
-    let nbsupp = 0,
+    let nbSupp = 0,
         nbLoop = Math.ceil(nbr/100),
         chan = message.channel
     try {
@@ -30,12 +30,12 @@ async function run (message, args) {
         try {
             msg = await chan.bulkDelete(cpt, true)
         } catch (e) {break}
-        nbsupp += msg.size
+        nbSupp += msg.size
         if (msg.size < 100) break
         }
 
     try {
-        let mess = await chan.send("<:text_3:483262304666779648> Vous venez de suprimer " + nbsupp + " messages")
+        let mess = await chan.send("🗑️ Vous venez de suprimer " + nbSupp + " messages")
         await mess.delete({timeout: 5000})
         }
     catch (e) {}

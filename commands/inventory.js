2a0466dd6afb934eb9d79784de4a612bf204ca5f
Zriox <37074290+zriox@users.noreply.github.com>
2021-02-17 09:33:28 +0100

    Round the % variation

--- commands/inventory.js
+++ commands/inventory.js
@@ -11,5 +11,5 @@ async function fct (message, args, client, db) {
     let inventory = await db.collection('members').findOne({id: message.member.id})
     if (!inventory) return message.channel.send("Ce joueur n'a pas d'inventaire.")
     let diff = (inventory.dailyBenefit || 0) - (inventory.dailyLoss || 0)
-    message.channel.send(`Vous possédez actuellement ${inventory.bolducs} Bolduc${inventory.bolducs > 1 ? 's' : ''} <:1B:805427963972943882> (${diff > 0 ? '+' :''}${diff} / augmentation de ${inventory.bolducs * 100 / (inventory.bolducs - diff)}% par rapport à hier)`)
+    message.channel.send(`Vous possédez actuellement ${inventory.bolducs} Bolduc${inventory.bolducs > 1 ? 's' : ''} <:1B:805427963972943882> (${diff > 0 ? '+' :''}${diff} / augmentation de ${Math.round(inventory.bolducs * 1000 / (inventory.bolducs - diff)) / 10}% par rapport à hier)`)
 }

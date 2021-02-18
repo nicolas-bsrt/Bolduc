a8d6831bfbf09b312f3d0a1be9f4b6d723653106
Zriox <37074290+zriox@users.noreply.github.com>
2021-02-18 23:37:53 +0100

    Do not display the % variation if start with 0

--- commands/inventory.js
+++ commands/inventory.js
@@ -10,6 +10,7 @@ module.exports = {
 async function fct (message, args, client, db) {
     let inventory = await db.collection('members').findOne({id: message.member.id})
     if (!inventory) return message.channel.send("Ce joueur n'a pas d'inventaire.")
-    let diff = (inventory.dailyBenefit || 0) - (inventory.dailyLoss || 0)
-    message.channel.send(`Vous possédez actuellement ${inventory.bolducs} Bolduc${inventory.bolducs > 1 ? 's' : ''} <:1B:805427963972943882>\n(${diff > 0 ? '+' :''}${diff} / augmentation de ${Math.round(inventory.bolducs * 1000 / (inventory.bolducs - diff)) / 10}% par rapport à hier)`)
+    let diff = (inventory.dailyBenefit || 0) - (inventory.dailyLoss || 0),
+        percent = inventory.bolducs - diff === 0 ? '' : ` / augmentation de ${Math.round(inventory.bolducs * 1000 / (inventory.bolducs - diff)) / 10}% par rapport à hier`
+    message.channel.send(`Vous possédez actuellement ${inventory.bolducs} Bolduc${inventory.bolducs > 1 ? 's' : ''} <:1B:805427963972943882>\n(${diff > 0 ? '+' :''}${diff}${percent})`)
 }

a6973cc178e867fc5aa62510cbfa0a858dcebf11
Zriox <37074290+zriox@users.noreply.github.com>
2021-02-18 23:47:51 +0100

    Be able to see other members inventory

--- commands/inventory.js
+++ commands/inventory.js
@@ -8,9 +8,17 @@ module.exports = {
 }
 
 async function fct (message, args, client, db) {
-    let inventory = await db.collection('members').findOne({id: message.member.id})
+    let target = message.member,
+        phrase = 'Vous possédez actuellement'
+    if (message.mentions.members) {
+        target = message.mentions.members.first()
+        phrase = target.displayName + ' possède'
+
+    }
+
+    let inventory = await db.collection('members').findOne({id: target.id})
     if (!inventory) return message.channel.send("Ce joueur n'a pas d'inventaire.")
     let diff = (inventory.dailyBenefit || 0) - (inventory.dailyLoss || 0),
         percent = inventory.bolducs - diff === 0 ? '' : ` / augmentation de ${Math.round(inventory.bolducs * 1000 / (inventory.bolducs - diff)) / 10}% par rapport à hier`
-    message.channel.send(`Vous possédez actuellement ${inventory.bolducs} Bolduc${inventory.bolducs > 1 ? 's' : ''} <:1B:805427963972943882>\n(${diff > 0 ? '+' :''}${diff}${percent})`)
+    message.channel.send(`${phrase} ${inventory.bolducs} Bolduc${inventory.bolducs > 1 ? 's' : ''} <:1B:805427963972943882>\n(${diff > 0 ? '+' :''}${diff}${percent})`)
 }

00d1519bca59396e1530b1faa0a577acba858e53
Zriox <37074290+zriox@users.noreply.github.com>
2021-02-20 21:13:43 +0100

    New daily variation display

--- commands/inventory.js
+++ commands/inventory.js
@@ -18,6 +18,7 @@ async function fct (message, args, client, db) {
     let inventory = await db.collection('members').findOne({id: target.id})
     if (!inventory) return message.channel.send("Ce joueur n'a pas d'inventaire.")
     let diff = (inventory.dailyBenefit || 0) - (inventory.dailyLoss || 0),
-        percent = inventory.bolducs - diff === 0 ? '' : ` / augmentation de ${Math.round(inventory.bolducs * 1000 / (inventory.bolducs - diff)) / 10}% par rapport à hier`
-    message.channel.send(`${phrase} ${inventory.bolducs} Bolduc${inventory.bolducs > 1 ? 's' : ''} <:1B:805427963972943882>\n(${diff > 0 ? '+' :''}${diff}${percent})`)
+        dailyVar = Math.round(inventory.bolducs * 1000 / (inventory.bolducs - diff)) / 10 - 100,
+        percent = inventory.bolducs - diff === 0 ? '' : ` / ${dailyVar > 0 ? '+' : ''}${dailyVar}%`
+    message.channel.send(`${phrase} ${inventory.bolducs} Bolduc${inventory.bolducs > 1 ? 's' : ''} <:1B:805427963972943882>\n(Variation journalière : ${diff > 0 ? '+' :''}${diff}${percent})`)
 }

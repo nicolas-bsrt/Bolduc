81aa85c84d68af57bf35ade90c72cd7cd85b30a5
Zriox <37074290+zriox@users.noreply.github.com>
2021-02-20 20:39:09 +0100

    Miscellaneous bug fix and advert in an embed

--- commands/pub.js
+++ commands/pub.js
@@ -1,3 +1,5 @@
+const Discord = require("discord.js")
+
 module.exports = {
     run: fct,
     conf: {
@@ -8,25 +10,36 @@ module.exports = {
 
 
 async function fct (message, args, client, db) {
-    let inventory = await db.collection('members').findOne({id: message.member.id}), pub, toRemove
+    let inventory = await db.collection('members').findOne({id: message.member.id}),
+        mention = `__Pub de ${message.author.tag}__ `,
+        pub,
+        toRemove
 
 
     if (args[0] === 'here') {
-        if (!inventory || !inventory.items.includes('PUB_H')) return message.channel.send("Vous n'avez pas acheté ce qu'il faut. Rendez-vous dans <#805560267219664946> pour ce faire.")
-        pub = message.content.slice(command.indexOf('here') + 4).trim()
+        if (!inventory || !inventory.items || !inventory.items.includes('PUB_H')) return message.channel.send("Vous n'avez pas acheté ce qu'il faut. Rendez-vous dans <#805560267219664946> pour ce faire.")
+        pub = message.content.slice(message.content.indexOf('here') + 4).trim()
         toRemove = 'PUB_H'
+        mention += '@here'
     }
     else if (args[0] === 'everyone') {
-        if (!inventory || !inventory.items.includes('PUB_E')) return message.channel.send("Vous n'avez pas acheté ce qu'il faut. Rendez-vous dans <#805560267219664946> pour ce faire.")
-        pub = message.content.slice(command.indexOf('everyone') + 8).trim()
+        if (!inventory || !inventory.items || !inventory.items.includes('PUB_E')) return message.channel.send("Vous n'avez pas acheté ce qu'il faut. Rendez-vous dans <#805560267219664946> pour ce faire.")
+        pub = message.content.slice(message.content.indexOf('everyone') + 8).trim()
         toRemove = 'PUB_E'
+        mention += '@everyone'
     }
     else {
-        if (!inventory || !inventory.items.includes('PUB')) return message.channel.send("Vous n'avez pas acheté ce qu'il faut. Rendez-vous dans <#805560267219664946> pour ce faire.")
+        if (!inventory || !inventory.items || !inventory.items.includes('PUB')) return message.channel.send("Vous n'avez pas acheté ce qu'il faut. Rendez-vous dans <#805560267219664946> pour ce faire.")
         pub = message.content.slice(5).trim()
         toRemove = 'PUB'
     }
 
-    client.channels.cache.get('805566128742072330').send(pub)
+
+    await client.channels.cache.get('805566128742072330').send(mention,
+        new Discord.MessageEmbed()
+            .setColor('#3b88c3')
+            .setDescription(pub)
+    )
+    await message.channel.send('✅ Pub envoyée.')
     await db.collection('members').updateOne({id: message.member.id}, {$pull: {items: toRemove}})
 }

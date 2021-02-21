a454125d9b457522219ca94b717d9203292d6d63
Zriox <37074290+zriox@users.noreply.github.com>
2021-02-21 21:43:17 +0100

    Round the result

--- commands/inventory.js
+++ commands/inventory.js
@@ -18,7 +18,7 @@ async function fct (message, args, client, db) {
     let inventory = await db.collection('members').findOne({id: target.id})
     if (!inventory) return message.channel.send("Ce joueur n'a pas d'inventaire.")
     let diff = (inventory.dailyBenefit || 0) - (inventory.dailyLoss || 0),
-        dailyVar = Math.round(inventory.bolducs * 1000 / (inventory.bolducs - diff)) / 10 - 100,
+        dailyVar = Math.round(inventory.bolducs * 1000 / (inventory.bolducs - diff) - 1000) / 10,
         percent = inventory.bolducs - diff === 0 ? '' : ` / ${dailyVar > 0 ? '+' : ''}${dailyVar}%`
     message.channel.send(`${phrase} ${inventory.bolducs} Bolduc${inventory.bolducs > 1 ? 's' : ''} <:1B:805427963972943882>\n(Variation journalière : ${diff > 0 ? '+' :''}${diff}${percent})`)
 }

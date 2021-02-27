f13eaef500acefd8489a40a4774a22229ed5d8e5
Zriox <37074290+zriox@users.noreply.github.com>
2021-02-27 18:20:16 +0100

    Spelling mistake

--- commands/adm-lottery.js
+++ commands/adm-lottery.js
@@ -23,7 +23,7 @@ async function fct (message, args, client, db) {
     if (lottery) return message.channel.send("Vous ne pouvez pas lancer deux méga-loteries en même temps, attendez sa fin ou annulez la avant d'en lancer une nouvelle.")
 
     if (!args[0] || isNaN(args[0]) || 0 > args[0]) return message.channel.send('Il faut me donner la durée de la méga-loterie (en minutes).')
-    if (args[0] > 1440) return message.channel.send('La méga-lotterie ne peut pas durer plus de 24h.')
+    if (args[0] > 1440) return message.channel.send('La méga-loterie ne peut pas durer plus de 24h.')
     if (!args[1] || isNaN(args[1])) return message.channel.send('Il faut me donner la somme de bolducs que vous souhaitez mettre en jeux pour cette loterie.')
     if (Number.isInteger(args[1]) || args[1] < 0) return message.channel.send('Le nombre de bolducs mis en jeux doit être un entier positif.')
 
@@ -62,13 +62,13 @@ async function add (reaction, user, db, tools) {
     await user.send(`Vous venez de vous inscrire dans une méga-loterie, tirage dans ${tools.howManyLast(new Date().getTime(), lottery.start)}.`)
 }
 async function cancel (message, args, client, db) {
-    // annule une méga-lotterie qu'on a lancé
+    // annule une méga-loterie qu'on a lancé
     let lottery = await db.collection('lotteries').findOne({id: message.member.id, type: 'megaLottery'})
-    if (!lottery) return message.channel.send("Vous n'avez lancé aucune lotterie, vous ne pouvez rien annuler.")
+    if (!lottery) return message.channel.send("Vous n'avez lancé aucune loterie, vous ne pouvez rien annuler.")
 
     await db.collection('members').updateMany({id: {$in: lottery.entrants}}, {$inc: {bolducs: lottery.amount, dailyLoss: -lottery.amount}})
     await db.collection('lotteries').deleteOne({id: lottery.id, type: 'megaLottery'})
-    message.channel.send('La méga-lotterie est annulé, les paris ont été reversés aux participants.')
+    message.channel.send('La méga-loterie est annulé, les paris ont été reversés aux participants.')
 }
 async function rem (reaction, user, db) {
     let lottery = await db.collection('lotteries').findOne({message: reaction.message.id, type: 'megaLottery'})
@@ -106,5 +106,5 @@ async function draw (message, db, client) {
     await db.collection('lotteries').deleteOne({id: lottery.id, type: 'megaLottery'})
     await db.collection('members').updateOne({id: winner.id}, {$inc: {bolducs: amount, dailyBenefit: amount}})
     await client.channels.cache.get('804768383626903552').send(`${winner} à remporté les Bolducs ! Soit ${amount} Bolducs <:1B:805427963972943882>`)
-    client.channels.cache.get('804480347592589312').send(`${winner.user.tag} a remporté ${amount} bolducs en gagnant la méga-lotterie.`)
+    client.channels.cache.get('804480347592589312').send(`${winner.user.tag} a remporté ${amount} bolducs en gagnant la méga-loterie.`)
 }
